a99acad0dbd6fa236d8baa0369e5e3dc0035a494
Marco <bortolanmarco@gmail.com>
2017-02-15 17:35:54 +0100

    get user announcements

--- src/api_wrapper.ts
+++ src/api_wrapper.ts
@@ -7,6 +7,12 @@ export function getAnnouncements() {
         payload: axios.get("https://api.bortolan.ml/announcements", { withCredentials: true })
     })
 }
+export function getUserannouncements() {
+    return ({
+        type: "MY_ANNOUNCEMENS",
+        payload: axios.get("https://api.bortolan.ml/user/announcements", { withCredentials: true })
+    })
+}
 export function login(phone: string, password: string) {
     return ({
         type: "LOGIN",
